3296d2ebb9bd46e40c706a315fd9b254cc5e58e4
PierceTee <iampiercetaylor@gmail.com>
2025-01-12 16:58:10 -0800

    script cleanup

--- workspace/scripts/service/install.sh
+++ workspace/scripts/service/install.sh
@@ -16,15 +16,4 @@ chmod +x /etc/init.d/led-settings-daemon
 
 echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: Finished writing service information to  $SERVICE_PATH ..."  >> $LOG_FILE
 
-# Manually create symlinks in /etc/rc.d
-
-#Start the service with a priority of 97.
-ln -s /etc/init.d/led-settings-daemon /etc/rc.d/S99led-settings-daemon
-
-#Stop the service with a priority of 03.
-ln -s /etc/init.d/led-settings-daemon /etc/rc.d/K03led-settings-daemon
-
-echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: Starting  $SERVICE_NAME ..."  >> $LOG_FILE
-/etc/init.d/led-settings-daemon start
-
 echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: exiting ..."  >> $LOG_FILE
--- workspace/scripts/service/uninstall.sh
+++ workspace/scripts/service/uninstall.sh
@@ -6,8 +6,7 @@ export LOG_FILE="/etc/led_controller/setting_daemon.log"
 
 echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: Removing all led_controller files from system ..."  >> $LOG_FILE
 /etc/init.d/led-settings-daemon stop
-rm /etc/rc.d/S98led-settings-daemon
-rm /etc/rc.d/K99led-settings-daemon
+rm /etc/rc.d/*led-settings-daemon
 rm /etc/init.d/led-settings-daemon
 rm -rf $SERVICE_PATH
 echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: exiting ..."  >> $LOG_FILE
