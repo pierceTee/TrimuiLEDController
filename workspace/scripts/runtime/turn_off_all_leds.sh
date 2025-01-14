fa79c4ef45e583df1a4316a0303b95c5cb72c53d
Pierce Taylor <43525848+pierceTee@users.noreply.github.com>
2025-01-13 22:59:50 -0800

    Add start menu with shortcuts. (#7)
    * Fix tests by removing cross compilation code.
    
    * cleanup deps target to fix tests.
    
    * Add menu rendering/functionality, fixed a small memory leak.
    
    * Clean up project/release directories. Fix daemon installation.

---
+++ workspace/scripts/runtime/turn_off_all_leds.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+export BASE_LED_PATH="/sys/class/led_anim"
+export GENERAL_BRIGHTNESS_PATH="$BASE_LED_PATH/max_scale"
+export FRONT_LED_BRIGHTNESS_PATH="$BASE_LED_PATH/max_scale_f1f2"
+export BACK_LED_BRIGHTNESS_PATH="$BASE_LED_PATH/max_scale_lr"
+export SCRIPT_NAME=$(basename "$0")
+
+chmod a-w $BASE_LED_PATH/*
+echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: Turning off all LEDs"
+echo 0 | tee $GENERAL_BRIGHTNESS_PATH $FRONT_LED_BRIGHTNESS_PATH $BACK_LED_BRIGHTNESS_PATH
---
+++ workspace/scripts/runtime/uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export SERVICE_PATH="/etc/led_controller"
+export SCRIPT_NAME=$(basename "$0")
+
+echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: Removing all led_controller files from system ..."
+/etc/init.d/led-settings-daemon stop
+rm /etc/rc.d/*led-settings-daemon
+rm /etc/init.d/led-settings-daemon
+rm -rf $SERVICE_PATH
+echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: exiting ..."
