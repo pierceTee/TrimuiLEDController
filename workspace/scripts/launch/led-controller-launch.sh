20d13c78d15a3b1d33c1c571a085352781500b07
PierceTee <iampiercetaylor@gmail.com>
2025-01-14 22:40:30 -0800

    Update daemon/script brightness logic to account for new top led brightness findings

--- workspace/scripts/launch/led-controller-launch.sh
+++ workspace/scripts/launch/led-controller-launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Becomes LedController/launch.sh
 export BASE_LED_PATH="/sys/class/led_anim"
-export GENERAL_BRIGHTNESS_PATH="$BASE_LED_PATH/max_scale"
+export TOP_BRIGHTNESS_PATH="$BASE_LED_PATH/max_scale"
 export FRONT_LED_BRIGHTNESS_PATH="$BASE_LED_PATH/max_scale_f1f2"
 export BACK_LED_BRIGHTNESS_PATH="$BASE_LED_PATH/max_scale_lr"
 export LOG_FILE="led_controller.log"
@@ -16,7 +16,7 @@ chmod a+w $BASE_LED_PATH/* >> $LOG_FILE
 
 # turn LEDS on
 echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: Turning on all LEDs"  >> $LOG_FILE
-echo 200 | tee $GENERAL_BRIGHTNESS_PATH $FRONT_LED_BRIGHTNESS_PATH $BACK_LED_BRIGHTNESS_PATH >> $LOG_FILE
+echo 200 | tee $TOP_BRIGHTNESS_PATH $FRONT_LED_BRIGHTNESS_PATH $BACK_LED_BRIGHTNESS_PATH >> $LOG_FILE
 
 echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: Launching led_controller..."  >> $LOG_FILE
 ./led_controller > led_controller.log 2>&1
