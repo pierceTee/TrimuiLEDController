0a686ffe42f4ef94bf6172ae4c0a8ed550a3bf15
PierceTee <iampiercetaylor@gmail.com>
2025-01-14 19:07:07 -0800

    update brightness to a scale of 10, update stock OS launch.sh to match current changes, fix duration UI spilling over screen

--- workspace/scripts/launch/led-controller-launch.sh
+++ workspace/scripts/launch/led-controller-launch.sh
@@ -1,27 +1,28 @@
 #!/bin/sh
-# Becomes LedController.pak/launch.sh
+# Becomes LedController/launch.sh
 export BASE_LED_PATH="/sys/class/led_anim"
-export LED_PATH="$BASE_LED_PATH/max_scale"
-export FRONT_LED_PATH="$BASE_LED_PATH/max_scale_f1f2"
-export BACK_LED_PATH="$BASE_LED_PATH/max_scale_lr"
+export GENERAL_BRIGHTNESS_PATH="$BASE_LED_PATH/max_scale"
+export FRONT_LED_BRIGHTNESS_PATH="$BASE_LED_PATH/max_scale_f1f2"
+export BACK_LED_BRIGHTNESS_PATH="$BASE_LED_PATH/max_scale_lr"
 export LOG_FILE="led_controller.log"
 
-echo "[`date '+%Y-%m-%d %H:%M:%S'`]: Launching cd $(dirname "$0")/led_controller" >> $LOG_FILE
+SCRIPT_NAME=$(basename "$0")
+
+echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: Launching cd $(dirname "$0")/led_controller" >> $LOG_FILE
 
 # Allow the application to change the LEDs
-echo "[`date '+%Y-%m-%d %H:%M:%S'`]: Enabling write permissions on $LED_PATH files"  >> $LOG_FILE
-chmod -v a+w $LED_PATH/* >> $LOG_FILE
+echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: Enabling write permissions on $BASE_LED_PATH files"  >> $LOG_FILE
+chmod a+w $BASE_LED_PATH/* >> $LOG_FILE
 
 # turn LEDS on
-echo "[`date '+%Y-%m-%d %H:%M:%S'`]: Turning on all LEDs"  >> $LOG_FILE
-echo 200 | tee $LED_PATH $FRONT_LED_PATH $BACK_LED_PATH >> $LOG_FILE
+echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: Turning on all LEDs"  >> $LOG_FILE
+echo 200 | tee $GENERAL_BRIGHTNESS_PATH $FRONT_LED_BRIGHTNESS_PATH $BACK_LED_BRIGHTNESS_PATH >> $LOG_FILE
 
-
-echo "[`date '+%Y-%m-%d %H:%M:%S'`]: Launching led_controller..."  >> $LOG_FILE
+echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: Launching led_controller..."  >> $LOG_FILE
 ./led_controller > led_controller.log 2>&1
 
 # Prevent other applications from changing the LEDs
-echo "[`date '+%Y-%m-%d %H:%M:%S'`]: Disabling write permissions on $LED_PATH files"  >> $LOG_FILE
-chmod -v a-w $LED_PATH/* >> $LOG_FILE
+echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: Disabling write permissions on $BASE_LED_PATH files"  >> $LOG_FILE
+chmod a-w $BASE_LED_PATH/* >> $LOG_FILE
 
-echo "[`date '+%Y-%m-%d %H:%M:%S'`]: exiting..."  >> $LOG_FILE
+echo "[`date '+%Y-%m-%d %H:%M:%S'`][$SCRIPT_NAME]: exiting..."  >> $LOG_FILE
